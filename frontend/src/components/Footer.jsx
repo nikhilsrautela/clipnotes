b6dc23a9aedbdccc50bdfc29f3212fd745a6fd83
Nikhil Rautela <nikhil.srautela@gmail.com>
2026-06-03 15:34:17 +0530

    fix: ui updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,23 +4,18 @@ export default function Footer(){
 <>  <hr class="border-t border-gray-500 mx-15"></hr>
 
 <footer className="bg-gray-100 dark:bg-white text-[#101828] dark:text-[#101828] py-8 font-ppeditorialold">
-  <div className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-6">
+      <div className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-6">
 
-    {/* Logo / Brand */}
-    <div>
-      <h2 className="text-2xl font-bold">clipnotes.</h2>
-      <p className="text-sm mt-1">
-        Smarter notes powered by AI.
-      </p>
-    </div>
+        <div>
+          <h2 className="text-2xl font-bold">clipnotes.</h2>
+          <p className="text-sm mt-1">a smarter way to learn</p>
+        </div>
+ 
+        <div className="text-sm text-center md:text-right">
+          © {new Date().getFullYear()} clipnotes.
+        </div>
 
-
-    {/* Copyright */}
-    <div className="text-sm text-center md:text-right">
-      © {new Date().getFullYear()} clipnotes.
-    </div>
-
-  </div>
+      </div>
   
 </footer>
 </>
